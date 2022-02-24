1b441b6fabbc00920c954a240071c65373603a94
Filippov Alex <alexey.filippov@picsart.com>
2022-02-24 17:36:10 +0700

    Initial Commit

---
+++ Echo/Echo.h
@@ -0,0 +1,18 @@
+//
+//  Echo.h
+//  Echo
+//
+//  Created by Филиппов Алексей on 24.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Echo.
+FOUNDATION_EXPORT double EchoVersionNumber;
+
+//! Project version string for Echo.
+FOUNDATION_EXPORT const unsigned char EchoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Echo/PublicHeader.h>
+
+
